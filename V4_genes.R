acdba4990d0307a1453435b58ff591912d3a7e45
Snehal Patel <25228411+sbpatel2009@users.noreply.github.com>
2019-02-26 11:37:13 -0500

    merge tables

--- V4_genes.R
+++ V4_genes.R
@@ -1,31 +1,78 @@
 library(readxl)
 
-#load StrataNGS version 3 gene list
+#######################################################
+##               READ IN NGS PANELS                  ##
+#######################################################
+
+#load StrataNGS version 3 panel
 StrataNGSv3 = read_xlsx(path = "Gene lists.xlsx", sheet = "StrataNGSv3")
 StrataNGSv3$StrataNGSv3 = "Yes"
-View(StrataNGSv3)
 
-#load Tempus xT gene list
+#load Tempus xT gene panel
 xT = read_xlsx(path = "Gene lists.xlsx", sheet = "xT")
+xT = xT[ , 1]
 xT$xT = "Yes"
-View(xT)
 
-#load Tempus xO gene list
+#load Tempus xO gene panel
 xO = read_xlsx(path = "Gene lists.xlsx", sheet = "xO")
 xO$xO = "Yes"
-View(xO)
 
-#load Illumina TSO 500 gene list
+#load Illumina TSO 500 gene panel
 TSO500 = read_xlsx(path = "Gene lists.xlsx", sheet = "TSO500")
+TSO500 = TSO500[TSO500$`Small variants` == "Yes", 1]
 TSO500$TSO500 = "Yes"
-View(TSO500)
 
-#load Illumina FoundationOne gene list
-FoundationOne = read_xlsx(path = "Gene lists.xlsx", sheet = "FoundationOne")
+#load Illumina FoundationOne gene panel
+FoundationOne = read_xlsx(path = "Gene lists.xlsx", sheet = "F_One")
 FoundationOne$FoundationOne = "Yes"
-View(FoundationOne)
 
-#load MSK-IMPACT gene list
+#load MSK-IMPACT gene panel
 MSK = read_xlsx(path = "Gene lists.xlsx", sheet = "MSK")
 MSK$MSK = "Yes"
-View(MSK)
+
+#Merge panels
+gene_list = merge(x = StrataNGSv3, y = xT, by = "Gene.Symbol", all = TRUE)
+gene_list = merge(x = gene_list, y = xO, by = "Gene.Symbol", all = TRUE)
+gene_list = merge(x = gene_list, y = TSO500, by = "Gene.Symbol", all = TRUE)
+gene_list = merge(x = gene_list, y = FoundationOne, by = "Gene.Symbol", all = TRUE)
+gene_list = merge(x = gene_list, y = MSK, by = "Gene.Symbol", all = TRUE)
+
+#save merged file
+write.csv(x = gene_list, file = "panel_comparison.csv")
+
+
+#######################################################
+##               READ IN CANCER CENSUS GENES         ##
+#######################################################
+
+#load Cacer Gene Census gene list
+CancerGeneCensus = read_xlsx(path = "Gene lists.xlsx", sheet = "CancerGeneCensus")
+CancerGeneCensus$CancerGeneCensus = "Yes"
+
+#load Cold Spring Harbor gene list
+ColdSpringHarbor = read_xlsx(path = "Gene lists.xlsx", sheet = "ColdSpringHarbor")
+ColdSpringHarbor$ColdSpringHarbor = "Yes"
+
+#load Vogelstein gene list
+Vogelstein = read_xlsx(path = "Gene lists.xlsx", sheet = "Vogelstein")
+Vogelstein$Vogelstein = "Yes"
+
+#load allOnco gene list
+allOnco = read_xlsx(path = "Gene lists.xlsx", sheet = "allOnco")
+allOnco$allOnco = "Yes"
+
+#load CCGD list
+CCGD = read_xlsx(path = "Gene lists.xlsx", sheet = "CCGD")
+CCGD$CCGD = "Yes"
+View(CCGD)
+
+
+#Merge lists
+gene_list = merge(x = CancerGeneCensus, y = ColdSpringHarbor, by = "Gene.Symbol", all = TRUE)
+gene_list = merge(x = gene_list, y = Vogelstein, by = "Gene.Symbol", all = TRUE)
+gene_list = merge(x = gene_list, y = allOnco, by = "Gene.Symbol", all = TRUE)
+gene_list = merge(x = gene_list, y = CCGD, by = "Gene.Symbol", all = TRUE)
+gene_list = merge(x = gene_list, y = StrataNGSv3, by = "Gene.Symbol", all = TRUE)
+
+#save merged file
+write.csv(x = gene_list, file = "panel_comparison.csv")
